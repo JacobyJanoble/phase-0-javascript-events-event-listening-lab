fdafae14f4075a19632dac767aa789cc6865bde4
Jacob <Jacob.McCann214@gmail.com>
2022-03-18 16:40:12 -0500

    Completed Assignment

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
-function addingEventListener() {
-}
+// function addingEventListener() {
+//     document.querySelector('#input').addEvenetListener('click', clickAlert)
+// }
+
+// const clickAlert = () => alert("I was clicked")
+
+function addingEventListener(){
+    const input = document.getElementById('input');
+    input.addEventListener('click', function() {
+      alert('I was clicked!');
+    });
+};
+addingEventListener();
+// function clickAlert(){
+//     return alert('Hi I was clicked!')
+// }
+
+
